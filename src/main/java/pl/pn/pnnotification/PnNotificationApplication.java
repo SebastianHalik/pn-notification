18a27aafc8d89c78eba784086ff2e9b7170fd37e
Sebastian Halik <sebastian.halik@bms.com.pl>
2024-06-15 13:03:26 +0200

    Initial

---
+++ src/main/java/pl/pn/pnnotification/PnNotificationApplication.java
@@ -0,0 +1,13 @@
+package pl.pn.pnnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PnNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PnNotificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/pn/pnnotification/PnNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pn.pnnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PnNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
